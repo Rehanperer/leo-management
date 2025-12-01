149ebdadd8b8218ada1e522803e0599baf461389
Rehan Perera <pererarehan2007@gmail.com>
2025-12-01 16:55:43 +0530

    feat: add `useParams` import from `next/navigation`

--- leo-management/app/dashboard/projects/[id]/page.tsx
+++ leo-management/app/dashboard/projects/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter, useParams } from 'next/navigation';
 import LoadingSpinner from '@/app/components/LoadingSpinner';
 import Link from 'next/link';
 
